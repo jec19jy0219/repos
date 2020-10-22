4d9b6b7276c2ebc951f96466b05b779aa065618e
19jy0219 <19jy0219@PC04.jynet2.jec.ac.jp>
2020-10-22 10:32:57 +0900

    mod 「Hello」を「HelloWorld」に変更 del ７行目を削除

--- project1/src/project1/HelloWorld.java
+++ project1/src/project1/HelloWorld.java
@@ -3,8 +3,7 @@ package project1;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello");
-		System.out.println("11111");
+		System.out.println("HelloWorld");
 	}
 
 }
